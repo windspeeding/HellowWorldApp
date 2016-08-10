6d1463122f7766acc09c0f1e9c65f4fbaef44a48
alexfengding <alexfengding@gmail.com>
2016-08-10 11:41:46 -0700

    Initial commit

---
+++ app/src/androidTest/java/com/equinix/ecp/hellowworldapp/ExampleInstrumentedTest.java
@@ -0,0 +1,27 @@
+package com.equinix.ecp.hellowworldapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.equinix.ecp.hellowworldapp", appContext.getPackageName());
+    }
+}
---
+++ app/src/main/java/com/equinix/ecp/hellowworldapp/ImportActivity.java
@@ -0,0 +1,14 @@
+package com.equinix.ecp.hellowworldapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
